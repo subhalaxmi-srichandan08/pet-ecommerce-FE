b112d1b43210acb577d09fc02591455ca1beb5e6
subhalaxmi-srichandan08 <subhalaxmisrichandan08@gmail.com>
2026-07-10 09:17:52 +0530

    api changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://pet-ecommerce-cq74.onrender.com/api",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
